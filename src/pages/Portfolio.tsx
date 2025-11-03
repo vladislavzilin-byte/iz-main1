313be8111626631a7cb6c3abb4bf70b08d6586e9
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-03 22:06:32 +0000

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Home from './pages/Home'
-import Shop from './pages/Shop'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Success from './pages/Success'
@@ -12,7 +10,6 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/success" element={<Success />} />
