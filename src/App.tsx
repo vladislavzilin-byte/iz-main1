42ca22c1122f4015741a74535cd2078ebbc106a8
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-03 15:59:03 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Portfolio from './pages/Portfolio'
 import Shop from './pages/Shop'
 import Training from './pages/Training'
 import Contacts from './pages/Contacts'
+import Home from './pages/Home'
 
 type Lang = 'lt' | 'en' | 'ru'
 const palette = { bg:'#000000' }
