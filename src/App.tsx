2394b66654e476e2fd37205a4ec185895496fceb
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-03 16:01:33 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import Portfolio from './pages/Portfolio'
 import Shop from './pages/Shop'
 import Training from './pages/Training'
 import Contacts from './pages/Contacts'
-import LoginBox from '../components/LoginBox'
-import { Link } from 'react-router-dom'
+import LoginBox from './components/LoginBox'
+
 
 type Lang = 'lt' | 'en' | 'ru'
 const palette = { bg:'#000000' }
