171acb206d71804f8641425e7b3ad1fd4c2316cb
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-03 22:08:55 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,10 @@ import App from './App'
 import './index.css'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 )
